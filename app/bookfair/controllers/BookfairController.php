9f4cae5857a07c5aebed0b9c7da4bd223bd2aef2
Russell Nash <russ.nash66@gmail.com>
2014-02-18 07:42:42 +1100

    Update bookfair opening and closing times

--- app/bookfair/controllers/BookfairController.php
+++ app/bookfair/controllers/BookfairController.php
@@ -90,20 +90,26 @@ class BookfairController extends BaseController {
     public function update($id) {
         if (Auth::user()->can('Update Bookfairs')) {
             try {
-                $bookfair = Bookfair::find($id);
+                $bookfair = Bookfair::with('salesTotals', 'totalAttendance')->find($id);
                 $bookfair->unguard();
                 $bookfair->year = Input::get('year');
                 $bookfair->season = Input::get('season');
                 $bookfair->location = Input::get('location');
                 $bookfair->start_date = Input::get('start_date'); // \DateTime::createFromFormat('Y-m-d', Input::get('start_date);
+                $bookfair->fri_open = Input::get('fri_open');
+                $bookfair->fri_close = Input::get('fri_close');
+                $bookfair->sat_open = Input::get('sat_open');
+                $bookfair->sat_close = Input::get('sat_close');
+                $bookfair->sun_open = Input::get('sun_open');
+                $bookfair->sun_close = Input::get('sun_close');
                 $bookfair->end_date = Input::get('end_date'); // \DateTime::createFromFormat('Y-m-d', Input::get('start_date);
-                $bookfair->duration = Input::get('duration');
+              //$bookfair->duration = Input::get('duration');
                 $bookfair->bag_sale = Input::get('bag_sale');
                 $bookfair->locked = Input::get('locked');
                 $bookfair->save();
                 // TODO:: Delete attendances before the open and after the close on each day
                 // start date must be a friday or saturday. end_date must be a sunday. No need to keep these input fields.
-                return Response::make($bookfair->toJson());
+                return $bookfair;
             } catch (Exception $e) {
                 // TODO: Pretty up the exception message. Currently its the SQL dump. Not pretty
                 return Response::make(json_encode(array(
