6cbb3976c3bea63d38a8b5e250bf87b7064a79f8
Russell Nash <russn@thedistillery.com.au>
2014-08-12 13:04:32 +1000

    Move to single column format with larger boxes and ticks for checked boxes instead of crosses.

--- app/bookfair/views/PalletPackingSheet.php
+++ app/bookfair/views/PalletPackingSheet.php
@@ -17,6 +17,17 @@ class PalletPackingSheet extends TCPDF {
     private $_units = 'mm';
     private $_pagesize = 'A4';
     private $_pageLen = 36;
+    private $_BoxesWidth = 0;
+    private $_LabelRowHeight = 6;
+    private $_LabelWidth = 24;
+    private $_TitleWidth = 170;
+    private $_LeftMargin = 8;
+    private $_RightMargin = 8;
+    private $_TicksPerLine = 25;
+    private $_TicksPerGroup = 5;
+    private $_LinesPerCell = 2;
+    private $_TicksCellHeight = 14;
+    private $_TicksPerCell = 50; // LinesPerCell * TicksPerLine
     private $_lineStyle;
     private $_fair;
     private $_prevSection;
@@ -35,9 +46,9 @@ class PalletPackingSheet extends TCPDF {
         $this->SetFont('dejavusans', 'B', 16, '', false);
         $this->SetFillColor(91, 43, 0, 45);
         $this->SetTextColor(0, 0, 0, 0);
-        $this->SetCellPaddings(1, 1, 0, 1);
+        $this->SetCellPaddings(1, 1, 0, 2);
         $bdr = 1;
-        $this->MultiCell(0, 8, $this->_header['section'], $bdr, 'L', true, 1);
+        $this->MultiCell(0, 0, $this->_header['section'], 1, 'L', true, 1);
         $this->SetTextColor(255, 255, 255, 0);
     }
 
@@ -62,7 +73,7 @@ class PalletPackingSheet extends TCPDF {
         $this->SetTitle('Pallet Packing Sheet');
         $this->SetSubject('Warehouse Stock Control');
         $this->SetAuthor(Auth::user()->name());
-        $this->setMargins(8, PDF_MARGIN_TOP, 8);
+        $this->setMargins($this->_LeftMargin, PDF_MARGIN_TOP, $this->_RightMargin);
         $this->SetHeaderMargin(PDF_MARGIN_HEADER);
         $this->SetFooterMargin(PDF_MARGIN_FOOTER);
         $this->_fair = & $bookfair;
@@ -80,6 +91,7 @@ class PalletPackingSheet extends TCPDF {
             $this->startPageGroup();
             $this->_footer = array('pallet'=>$pallet->name);
         } else {
+           $this->Line($this->_LeftMargin, $this->getY(), 202, $this->getY());
            $this->_header['pallet'] = $pallet->name;
            $this->_header['section'] = $section->name;
            if ($this->_header['pallet'] <> $this->_footer['pallet']) {
@@ -91,108 +103,90 @@ class PalletPackingSheet extends TCPDF {
     }    
 
 
-    private function newtickboxes($boxes, $checks = 0) {
+    private function tickboxes($boxes, $checks = 0) {
+        $cell = array();
         $ticks = '';
         for ($i = 1; $i <= $boxes; $i++) {
-            $ticks .= ($i > $checks) ? $this->unichr(9633) : $this->unichr(9632);
-            if ($i%20 == 0 && $i < $boxes) {
+            $ticks .= ($i > $checks) ? $this->unichr(9744) : $this->unichr(9745);
+            //$ticks .= ($i > $checks) ? $this->unichr(9633) : $this->unichr(9632);
+            if ($i%$this->_TicksPerLine === 0 && $i < $boxes) {
                 $ticks .= "\n";
+                if ($i%$this->_TicksPerCell === 0 && $i < $boxes) {
+                    $cell[] = $ticks;
+                    $ticks = '';
+                }
             } else {
-                if ($i%5 == 0) { $ticks .= '  '; }
+                if ($i%$this->_TicksPerGroup === 0) { $ticks .= '  '; }
             }
         }
-        return $ticks;
-    }
-    
-    public function formatCell( $target = null ) {
-        if (!is_null($target)) {
-            return array(
-                'label'   => $target->label,
-                'heading' => $target->name, 
-                'boxes'   => $target->target,
-                'ticks'   => $this->newtickboxes($target->target, $target->packed));
-        } else {
-            return array(
-                'label'   => '',
-                'heading' => '',
-                'boxes'   => 0,
-                'ticks'   => '');                
+        if (!$ticks == '') {
+            $cell[] = $ticks;
         }
+        return $cell;
     }
-    
-    public function printLabel($text, $ln = 0) {
-        $bdr = 'TL'; $ln = 0;
-        if (strlen($text) < 6 ) {
-            $this->SetFont('dejavusans', 'B', 12, '', false);
-        } else {
-            $this->SetFont('dejavusans', 'B', 11-(strlen($text)-4), '', false);
-        }
-        $this->SetCellPaddings(2, 2, 0, 0);
-        $this->MultiCell(18, 9, $text, $bdr, 'L', false, $ln, '', '', true, 0, false, false, 16, 'T', false);
-        //$this->Cell(18, 10, $text, $bdr, $ln,  'L', false, '', 0, false, 'T', 'M');        
-    }
-    
-    public function printHeading($text, $ln = 0) {
-        $bdr = 'TR'; $stretch = 1;
-        $this->SetFont('dejavusans', 'N', 12, '', false);
-        $this->SetCellPaddings(0, 2, 0, 0);
-        //$this->Cell(80, 0, $text, $bdr, $ln,  'L', false, '', 1, false, 'T', 'M');        
-        $this->MultiCell(79, 9, $text, $bdr, 'L', false, $ln, '', '', true, $stretch, false, false, 16, 'T', false);
-    }
-    
-    public function printCheckboxes($text, $h, $ln = 0) {
-        $bdr = ($ln == 0) ? 'BLR' : 'RB';
-        $this->SetCellPaddings(8, 0, 0, 0);
-        $this->SetFontSize(13.5);
-        $this->SetFontSpacing(-0.5);
-        $this->MultiCell(97, $h, $text, $bdr, 'L', false, $ln, '', '', true, 0, false, false, 0, 'M', false);
+          
+    public function printCheckboxes($text, $h = 14) {
+        $this->SetCellPaddings(2, 0, 0, 0);
+        $this->SetFontSize(20);
+        $this->SetFontSpacing(-0.8);
+        $this->SetCellHeightRatio(0.85);
+        $this->MultiCell($this->_BoxesWidth, $h, $text, 'LR', 'L', false, 1, '', '', true, 0, false, false, 0, 'M', false);
         $this->SetFontSpacing(0);
+        $this->SetCellHeightRatio(1.25);
     }    
     
     public function rowheight($boxes) {
         // Typical row has 10mm for Heading, 6mm per row of checkboxes and 
         // 4mm whitespace at the bottom of the box.
-        return 6 * ceil($boxes /20) + 4;
+        return 4 + $this->_LabelRowHeight * (1 + ceil($boxes / $this->_TicksPerLine));
+    }  
+
+    private function printTarget($target) {
+        $c = ($target->target % $this->_TicksPerCell);
+        $h = (($target->target === 0) || $c === 0 || ($c > $this->_TicksPerLine)) ? $this->_TicksCellHeight : ($this->_TicksCellHeight / 2);
+        $text = 'Target: ' . $target->target;
+        $this->SetCellHeightRatio(0.85);
+        $this->SetCellPaddings(2, 2, 2, 2);
+        $this->SetFontSize(8);
+        $this->MultiCell($this->_BoxesWidth, $h, $text, 'LR', 'R', false, 1, '', '', true, 0, false, true, $h, 'B', false);
+        $this->SetCellHeightRatio(1.25);
     }
     
-    public function printRow($cols) {        
-        if (count($cols) == 1) {
-            $cols[] = $this->formatCell();
+    
+    private function printCategory($target) {
+// MultiCell($w,$h,$txt,$border = 0,$align = 'J',$fill = false,$ln = 1,$x = '',$y = '',$reseth = true,$stretch = 0,$ishtml = false,$autopadding = true,$maxh = 0,$valign = 'T',$fitcell = false )
+        $h = $this->_LabelRowHeight;
+        $this->SetCellPaddings(2, 2, 0, 3);
+        $this->SetFont('dejavusans', 'B', 12, '', false);
+        $this->MultiCell($this->_LabelWidth, $h, $target->label, 'L', 'L', false, 0, '', '', true, 0, false, true, 0, 'T', false);
+        $this->SetCellPaddings(0, 2, 0, 3);
+        $this->SetFont('dejavusans', 'N', 12, '', false);
+        $this->MultiCell($this->_TitleWidth, $h, $target->name, 'R', 'L', false, 1, '', '', true, 0, false, true, 0, 'T', false);
+         
+        $boxes = $this->tickboxes($target->target, $target->packed);        
+        foreach ($boxes as $line) {
+            $this->printCheckboxes($line);
         }
-        $height = $this->rowheight(max($cols[0]['boxes'], $cols[1]['boxes']));
-        if (($this->getY() + $height) > 255) {
-            $this->AddPage();
-        }
-        $this->printLabel($cols[0]['label']);
-        $this->printHeading($cols[0]['heading']);
-        $this->printLabel($cols[1]['label']);
-        $this->printHeading($cols[1]['heading'], 1);
-        $this->printCheckboxes($cols[0]['ticks'], $height);
-        $this->printCheckboxes($cols[1]['ticks'], $height, 1);
-        $this->Line(10, $this->getY(), 202, $this->getY());
-    }    
-
+        $this->printTarget($target);
+        $this->Line($this->_LeftMargin, $this->getY(), 202, $this->getY());       
+    }
+    
     public function Render($targets) {
         $this->SetFont('dejavusans', '', 12, '', false);
-        $cells = array(); $col = 0; $pallet = 0; $section = 0;
+        $pallet = 0; $section = 0;
         foreach($targets as $target) {
             if (isset($target->pallet)) {
                 if ($target->pallet_id <> $pallet || $target->category->section_id <> $section) {
-                    if ($col > 0) {
-                        $this->printRow($cells);
-                    }
                     $this->newPage($target->pallet, $target->category->section);
-                    $col = 0;
-                    $cells = array();                           
                     $pallet = $target->pallet_id;
                     $section = $target->category->section_id;
-                }
-                $cells[$col] = $this->formatCell($target);
-                if ($col == 1) {
-                    $this->printRow($cells);
-                    $cells = array();                           
-                }
-                $col = ($col == 1) ? 0 : 1;
+                } else {
+                    $height = $this->rowheight($target->target);
+                    if (($this->getY() + $height) > 255) {
+                        $this->AddPage();
+                    }                
+                }        
+                $this->printCategory($target);
             }
         }
     }  
